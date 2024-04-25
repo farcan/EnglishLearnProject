76ea873cd62e93abc5ef60673a49cb423449d56b
Fahri Gedik <fahrican9978@gmail.com>
2024-04-25 14:30:18 +0300

    First Commit
    First Commit

---
+++ EnglishLearningProject/EnglishLearningProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EnglishLearningProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
